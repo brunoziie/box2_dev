3159461ea7fa90619289e82cdd7d15f8e0affb03
brunoziie <bruno@studiopixus.com>
2012-11-04 19:07:56 -0300

    adding docs

---
+++ docs/api.js
@@ -0,0 +1,22 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Box2",
+        "Box2.Ajax",
+        "Box2.Configs",
+        "Box2.Controller",
+        "Box2.Datasource.SQLite",
+        "Box2.Device",
+        "Box2.Forms",
+        "Box2.HTMLEntities",
+        "Box2.History",
+        "Box2.Inflector",
+        "Box2.Loader",
+        "Box2.Mapper",
+        "Box2.Model",
+        "Box2.View"
+    ],
+    "modules": [],
+    "allModules": []
+} };
+});
